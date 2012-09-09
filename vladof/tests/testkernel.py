6db2710a3b9a288b8028617ffcd2ba74a0827714
Adam Washington <adlwashi@indiana.edu>
2012-09-09 18:48:45 -0400

    Added first unit tests for kernel code.

---
+++ vladof/tests/testkernel.py
@@ -0,0 +1,35 @@
+import unittest
+import numpy
+import node
+
+def test_function(x,y):
+    return (x**y+y/x)*(x-y*2)
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        x = numpy.random.rand(50000).astype(numpy.float32)
+        y = numpy.random.rand(50000).astype(numpy.float32)
+
+        self.x = x
+        self.y = y
+
+        self.clx = node.Data(x,"x")
+        self.cly = node.Data(y,"y")
+
+    def test_sum(self):
+        clz = self.clx+self.cly
+        z = clz.compile("sum").run()
+        self.assertTrue(numpy.all(z==self.x+self.y))
+
+    def test_weird(self):
+        clz = test_function(self.clx,self.cly)
+        z = clz.compile("sum").run()
+        t = test_function(self.x,self.y)
+        max = numpy.max(numpy.abs((z-t)/t))
+        index = numpy.abs((z-t)/t)==max
+        #Since the GPU does 32-bit math while the processor uses 80
+        self.assertTrue(numpy.mean(((z-t)/t)**2)**0.5<5e-7)
+
+if __name__ == '__main__':
+    unittest.main()
